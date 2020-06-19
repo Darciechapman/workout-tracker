b70ae4cd1f3c597279e84f120bd26645a09ace51
Darciechapman <darciechapman96@gmail.com>
2020-06-19 20:04:19 +0800

    fixing __dirname syntax

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -3,15 +3,15 @@ const path = require("path");
 
 //HTML routes
 router.get("/", (req, res) => {
-    res.sendFile(path.join(_dirname + "/../public/index.html"))
+    res.sendFile(path.join(__dirname + "/../public/index.html"))
 })
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(+dirname + '/../public/index.html'))
+    res.sendFile(path.join(__dirname + '/../public/index.html'))
 })
 
-router.get("exercise", (req, res) => {
-    res.sendFile(path.join(_dirname + "/../public/exercise.html"))
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../public/exercise.html"))
 })
 
 module.exports = router;
